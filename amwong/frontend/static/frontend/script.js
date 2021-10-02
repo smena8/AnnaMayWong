6ad65018c1313db43e39dc3e4d097af50f3f0a39
smena8 <puttinonthefritz@protonmail.com>
2021-10-01 21:12:21 -0700

    continued work on parallax scroll, fixed more of the mobile css styling

--- amwong/frontend/static/frontend/script.js
+++ amwong/frontend/static/frontend/script.js
@@ -1,3 +1,13 @@
+
+
+document.querySelector("a[href='#bio']").addEventListener('click', scrollToBio)
+
+function scrollToBio() {
+    console.log('clicked')
+    document.querySelector('#bio').scrollIntoView();
+}
+
+
 window.addEventListener('scroll', function(e) {
 
     const targets = document.querySelectorAll('.scroll');
@@ -8,16 +18,35 @@ window.addEventListener('scroll', function(e) {
         var posY = window.scrollY * target.dataset.ratey;
         var dir = target.dataset.direction
 
-        if ((dir === 'left') || (dir === 'up')) {
-            posX = posStart - posX
-        } else {
-            posX = posStart + posX
+        if (posX) {
+            if (dir === 'left') {
+                posX = posStart - posX
+            } else {
+                posX = posStart + posX
+            }
         }
+
+        if (posY) {
+            if (dir === 'up') {
+                posY = posStart - posY
+            } else {
+                posY = posStart + posY
+            }
+        }
+
 
         if ((posStart > 0) && (posX < 0)) {
             posX = 0
         } else if ((posStart < 0) && (posX > 0)){
             posX = 0
+        }
+
+        if (target.className != 'gallery-column scroll') {
+            if ((posStart > 0) && (posY < 0)) {
+                posY = 0
+            } else if ((posStart < 0) && (posY > 0)){
+                posY = 0
+            }
         }
         target.style.transform = 'translate3d('+posX+'px,'+posY+'px, 0px)';
 
